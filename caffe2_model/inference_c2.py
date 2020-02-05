490883db7fff2f5c83e9c2bb4df9bf175f3fe6ba
Ashwin <ash1995@gmail.com>
2020-02-05 07:14:16 -0500

    Add cfg dependency

--- caffe2_model/inference_c2.py
+++ caffe2_model/inference_c2.py
@@ -4,6 +4,7 @@ from caffe2.proto import caffe2_pb2
 from caffe2.python import core, workspace, models
 import torch
 
+from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
 from detectron2.data import MetadataCatalog
 import detectron2.data.transforms as T
@@ -13,15 +14,40 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 import sys
 sys.path.insert(0,"/home/an1/detectron2")
 
+def setup_cfg(args):
+    # load config from file and command-line arguments
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    cfg.freeze()
+    return cfg
 
 def get_parser():
-    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
+    parser = argparse.ArgumentParser(description="Detectron2 caffe2 demo")
+
+    parser.add_argument(
+        "--config-file",
+        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
 
     parser.add_argument(
         "--confidence-threshold",
         type=float,
         default=0.5,
         help="Minimum score for instance predictions to be shown",
+    )
+
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
     )
     return parser
 
@@ -37,11 +63,15 @@ if __name__ == "__main__":
 
     args = get_parser().parse_args()
 
+    cfg = setup_cfg(args)
+
     IMAGE_LOCATION = "/home/an1/detectron2/caffe2_model/input.jpg"
     INIT_NET = '/home/an1/detectron2/caffe2_model/model_init.pb'
     PREDICT_NET = '/home/an1/detectron2/caffe2_model/model.pb'
 
-    transform_gen = T.ResizeShortestEdge([800, 800], 1333)
+    transform_gen = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
 
     img, img_tensor = load_img(IMAGE_LOCATION, transform_gen)
 
@@ -81,7 +111,9 @@ if __name__ == "__main__":
     results[0]['instances'].scores = torch.index_select(
         results[0]['instances'].scores, 0, indices)
 
-    metadata = MetadataCatalog.get('coco_2017_val')
+    metadata = MetadataCatalog.get(
+            cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+        )
 
     img = img[:, :, ::-1]
     visualizer = Visualizer(img, metadata, instance_mode=ColorMode.IMAGE)
