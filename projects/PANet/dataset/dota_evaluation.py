11c5480cbfb139aa0369c93a45afecb18fb87ae5
Ashwin <ash1995@gmail.com>
2020-05-07 04:52:41 -0400

    Update DOTA Evaluator
    Added functionality for saving dota_val results in the dota format. Now you can perform dota evaluation via the Dota devkit. First merge the result via ResultMerge.py and then run dota-v1.5_evaluation_task1.py

--- projects/PANet/dataset/dota_evaluation.py
+++ projects/PANet/dataset/dota_evaluation.py
@@ -5,8 +5,10 @@ import io
 import itertools
 import json
 import logging
+import math
 import numpy as np
 import os
+import shutil
 import torch
 from collections import OrderedDict, defaultdict
 import detectron2.utils.comm as comm
@@ -50,6 +52,9 @@ class DOTAEvaluator(RotatedCOCOEvaluator):
         self._metadata = MetadataCatalog.get(dataset_name)
         self._logger.info("Converting {} to COCO format".format(dataset_name))
 
+        # Save results in DOTA format
+        self.save_dota = True
+
         cache_path = os.path.join(output_dir, f"{dataset_name}_coco_format.json")
         convert_to_coco_json(dataset_name, cache_path)
 
@@ -61,6 +66,137 @@ class DOTAEvaluator(RotatedCOCOEvaluator):
         # performed using the COCO evaluation server).
         self._do_evaluation = "annotations" in self._coco_api.dataset
         assert self._do_evaluation is True
+
+    def process(self, inputs, outputs):
+        """
+        Args:
+            inputs: the inputs to a COCO model (e.g., GeneralizedRCNN).
+                It is a list of dict. Each dict corresponds to an image and
+                contains keys like "height", "width", "file_name", "image_id".
+            outputs: the outputs of a COCO model. It is a list of dicts with key
+                "instances" that contains :class:`Instances`.
+        """
+
+        self._dota_preds = defaultdict(list)
+
+        for input, output in zip(inputs, outputs):
+            image_id = input["image_id"]
+            prediction = {"image_id": image_id}
+
+            if "instances" in output:
+                instances = output["instances"].to(self._cpu_device)
+
+                boxes = instances.pred_boxes.tensor.numpy()
+                scores = instances.scores.tolist()
+                classes = instances.pred_classes.tolist()
+
+                for box, score, cls in zip(boxes, scores, classes):
+                    quad = xywha_to_quad(box)
+                    # Flatten list of tuples 
+                    quad = list(sum(quad, ()))
+                    assert len(quad) == 8, "Invalid quadrilateral"
+                    self._dota_preds[cls].append(
+                        f"{image_id} {score:.3f} {quad[0]:.1f} {quad[1]:.1f} {quad[2]:.1f} {quad[3]:.1f}" \
+                            f" {quad[4]:.1f} {quad[5]:.1f} {quad[6]:.1f} {quad[7]:.1f}"
+                    )
+
+                prediction["instances"] = self.instances_to_json(instances, input["image_id"])
+                prediction["dota_instances"] = self._dota_preds
+            if "proposals" in output:
+                prediction["proposals"] = output["proposals"].to(self._cpu_device)
+
+            
+            self._predictions.append(prediction)
+
+    def evaluate(self):
+        if self._distributed:
+            comm.synchronize()
+            predictions = comm.gather(self._predictions, dst=0)
+            predictions = list(itertools.chain(*predictions))
+
+            if not comm.is_main_process():
+                return {}
+        else:
+            predictions = self._predictions
+
+        if len(predictions) == 0:
+            self._logger.warning("[COCOEvaluator] Did not receive valid predictions.")
+            return {}
+
+        dota_det_dict = dict((k, []) for k in range(len(self._metadata.thing_classes)))
+        for p in predictions:
+            for k in p['dota_instances'].keys():
+                dota_det_dict[k] += p['dota_instances'][k]
+            p.pop('dota_instances')
+
+        if self._output_dir:
+            PathManager.mkdirs(self._output_dir)
+            file_path = os.path.join(self._output_dir, "instances_predictions.pth")
+            with PathManager.open(file_path, "wb") as f:
+                torch.save(predictions, f)
+            
+            if self.save_dota:
+                dirpath = os.path.join(self._output_dir, "dota_dets")
+                if PathManager.exists(dirpath) is False:
+                    PathManager.mkdirs(dirpath)
+                else:
+                    shutil.rmtree(dirpath)
+                    PathManager.mkdirs(dirpath)
+                # Write class detections
+                for cls_id, cls_name in enumerate(self._metadata.thing_classes):
+                    dfilename = os.path.join(dirpath, f'Task1_{cls_name}.txt')
+                    with open(dfilename, 'w') as j:
+                        for p in dota_det_dict[cls_id]:
+                            j.write("%s\n" % p)
+
+        self._results = OrderedDict()
+        if "proposals" in predictions[0]:
+            self._eval_box_proposals(predictions)
+        if "instances" in predictions[0]:
+            self._eval_predictions(set(self._tasks), predictions)
+        # Copy so the caller can do whatever with results
+        return copy.deepcopy(self._results)
+
+    def _eval_predictions(self, tasks, predictions):
+        """
+        Evaluate predictions on the given tasks.
+        Fill self._results with the metrics of the tasks.
+        """
+        self._logger.info("Preparing results for COCO format ...")
+        coco_results = list(itertools.chain(*[x["instances"] for x in predictions]))
+
+        # unmap the category ids for COCO
+        if hasattr(self._metadata, "thing_dataset_id_to_contiguous_id"):
+            reverse_id_mapping = {
+                v: k for k, v in self._metadata.thing_dataset_id_to_contiguous_id.items()
+            }
+            for result in coco_results:
+                result["category_id"] = reverse_id_mapping[result["category_id"]]
+
+        if self._output_dir:
+            file_path = os.path.join(self._output_dir, "coco_instances_results.json")
+            self._logger.info("Saving results to {}".format(file_path))
+            with PathManager.open(file_path, "w") as f:
+                f.write(json.dumps(coco_results))
+                f.flush()
+
+        if not self._do_evaluation:
+            self._logger.info("Annotations are not available for evaluation.")
+            return
+
+        self._logger.info("Evaluating predictions ...")
+        for task in sorted(tasks):
+            assert task == "bbox", "Task {} is not supported".format(task)
+            coco_eval = (
+                self._evaluate_predictions_on_coco(self._coco_api, coco_results)
+                if len(coco_results) > 0
+                else None  # cocoapi does not handle empty results very well
+            )
+
+            res = self._derive_coco_results(
+                coco_eval, task, class_names=self._metadata.get("thing_classes")
+            )
+            self._results[task] = res
 
 
 def convert_to_coco_json(dataset_name, output_file, allow_cached=True):
@@ -182,3 +318,16 @@ def convert_to_coco_dict(dataset_name):
         "licenses": None,
     }
     return coco_dict
+
+def xywha_to_quad(bbox):
+    """
+    Return list of tuples of quadrilateral points
+    """
+    cnt_x, cnt_y, w, h, angle = bbox
+    theta = angle * math.pi / 180.0
+    c = math.cos(theta)
+    s = math.sin(theta)
+    rect = [(-w / 2, h / 2), (-w / 2, -h / 2), (w / 2, -h / 2), (w / 2, h / 2)]
+    # x: left->right ; y: top->down
+    rotated_rect = [(s * yy + c * xx + cnt_x, c * yy - s * xx + cnt_y) for (xx, yy) in rect]
+    return rotated_rect
